2b78fc9263d9b883ffeb9c9cf8f3f8b774820040
chenghao525 <killersun23@163.com>
2020-03-02 14:04:59 -0500

    First commit

---
+++ quizHero_SpringBoot/quizHero_springBoot/src/main/java/com/chenghao/demo/QuizHeroApplication.java
@@ -0,0 +1,13 @@
+package com.chenghao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizHeroApplication.class, args);
+	}
+
+}
